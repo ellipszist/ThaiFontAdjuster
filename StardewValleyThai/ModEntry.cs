071154545f2f185d6bef1725d2bf3921f12c7570
ELL <ellipszist@gmail.com>
2024-04-14 22:12:03 +0700

    Thai Font Adjuster

---
+++ StardewValleyThai/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+namespace StardewValleyThai
+{
+    internal class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            ThaiFontFix.Init();
+        }
+    }
+}
